dc0e64ea735058aa71760483250f17b08f134bac
Amarea <Amarea1103@gmail.com>
2026-03-05 21:22:25 -0500

    implement writeMessage(msg) Securely

--- script.js
+++ script.js
@@ -18,12 +18,22 @@ recognition.start();
 
 // Capture User Speech
 function onSpeak(event) {
-    console.log(event);
-    const msg= event.results[0][0].transcript;
-
-    console.log(msg);
+  const msg = event.results[0][0].transcript;
+  writeMessage(msg);
 }
 
 // Listen to and handle the speech event
 recognition.addEventListener('result', onSpeak);
+
+// See in the DOM what the user has spoken
+function writeMessage(msg) {
+  msgEl.innerHTML = '';
+  const div = document.createElement('div');
+  div.textContent = 'You said: ';
+  const span = document.createElement('span');
+  span.classList.add('box');
+  span.textContent = msg;
+
+  msgEl.append(div, span);
+}
 
